3d850273d81e3dd4edf7c91b4e1c1f5ad5f4e724
Jake Clarke <jake.clarke@secnewgate.com.au>
2023-12-03 12:05:24 +1100

    format, add error logging, change to once a week run

--- liquor_licence_applications_vic_geocode.py
+++ liquor_licence_applications_vic_geocode.py
@@ -1,4 +1,5 @@
 """Code to geocode the address field from the liquor licence applications"""
+import json
 import time
 import pandas as pd
 import os
@@ -10,31 +11,51 @@ from dotenv import load_dotenv
 
 from azure_maps_geocode import AzureMapsGeocode
 
+# where is the input file?
 load_dotenv()
 
 OUTPUT_DIRECTORY = os.getenv("OUTPUT_DIRECTORY")
 OUTPUT_FILENAME = os.getenv("OUTPUT_FILENAME")
-AZURE_SUBSCRIPTION_KEY = os.getenv("AZURE_SUBSCRIPTION_KEY")
 
-amg = AzureMapsGeocode()
-
-
-# read output
 location = f"{OUTPUT_DIRECTORY}{os.sep}{OUTPUT_FILENAME}.csv"
+errors_filepath = f"{OUTPUT_DIRECTORY}{os.sep}geocode-errors.json"
 
+# load input
 input = pd.read_csv(location)
 results = []
+
+# create geocode object
+amg = AzureMapsGeocode()
+
+# place for errors
+errors = []
+
+# iterate over data
 for index, row in input.iterrows():
-    if index % 10 == 0:
+    # every 100th run, sleep - to not spam API
+    if index % 100 == 0:
         time.sleep(10)
 
-    result = amg.geocode_address(row['id'], row['Premises Address'])
-    results.append(result)
-    
+    # geocode address
+    try:
+        result = amg.geocode_address(row["id"], row["Premises Address"])
+        results.append(result)
+    except:
+        errors.append(result)
+        print(result)
+
+with open(errors_filepath, "w") as final:
+    json.dump(errors, final)
 
 results_df = pd.DataFrame(results)
 
-output = pd.merge(left=input, right=results_df, left_on="Premises Address", right_on='input_address', how="left")
+output = pd.merge(
+    left=input,
+    right=results_df,
+    left_on="Premises Address",
+    right_on="input_address",
+    how="left",
+)
 output.to_csv(f"{OUTPUT_DIRECTORY}{os.sep}{OUTPUT_FILENAME}-geocoded.csv")
 
 geo_json = to_geojson(
